45ba519780b230999bffe2ce3df13c36fc999419
hagar-rashed <hagerrashed1@gmail.com>
2021-06-13 13:05:33 +0200

    create project

---
+++ table/table-2/table.php
@@ -0,0 +1,118 @@
+<?php 
+
+
+$users = [
+    (object)[
+        'id' => 1,
+        'name' => 'ahmed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'football', 'swimming', 'running'
+        ],
+        'activities' => [
+            "school" => 'drawing',
+            'home' => 'painting'
+        ],'email'=>['ahmed@gmail.com','test@test.com']
+    ],
+    (object)[
+        'id' => 2,
+        'name' => 'mohamed',
+        "gender" => (object)[
+            'gender' => 'm'
+        ],
+        'hobbies' => [
+            'swimming', 'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],'email'=>['mohamed@gmail.com','test@test.com','test@test.com']
+
+    ],
+    (object)[
+        'id' => 3,
+        'name' => 'mena',
+        "gender" => (object)[
+            'gender' => 'f'
+        ],
+        'hobbies' => [
+            'running',
+        ],
+        'activities' => [
+            "school" => 'painting',
+            'home' => 'drawing'
+        ],'email'=>[]
+    ]
+];
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Table/2</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <table class="table">
+  <thead>
+    <tr>
+    <?php 
+       foreach($users[0] AS $key=>$value){
+           echo "<th>" . $key . "</th>" ;
+       }
+    ?>
+    </tr>
+  </thead>
+  <tbody>
+     
+    <?php 
+      foreach($users AS $key=>$value){ ?>
+         <tr>
+     <?php
+     foreach($value AS $key1=>$value1){
+        ?>
+        <td>
+     <?php
+     if(gettype($value1) != 'array' && gettype($value1) != 'object'){
+         echo $value1;
+     }else{
+         foreach($value1 AS $key2=>$value2){
+             if($key2 == 'gender' ){
+                 if( $value2 == 'm' ){
+                 $value2 = "male"; //comm
+                 }else{
+                 $value2 = "fmale"; //comm
+                 }
+             }
+             echo $value2 ." / ";
+         } 
+        
+   } ?>
+     
+     </td> 
+    <?php }  ?>
+        
+     </tr>
+     <?php }?>
+
+
+
+  </tbody>
+</table>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
